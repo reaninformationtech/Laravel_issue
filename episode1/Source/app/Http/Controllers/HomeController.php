b483c8fad290254091b817b223085e073744075a
Pu.IT <82702646+pu-informationtechnology@users.noreply.github.com>
2021-06-14 21:18:37 +0700

    Episode 1

---
+++ episode1/Source/app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\DataModel;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $menu=new DataModel();
+
+        $leftmenu=$menu->getmenu();
+
+      //  dd($leftmenu);
+
+        return view('home',$leftmenu);
+    }
+}
